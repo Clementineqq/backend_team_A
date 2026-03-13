ffe95ba6d0ac07b278174fcc13c05f987d9f2473
Clementine <mjchi023@gmail.com>
2026-03-13 21:44:22 +0400

    jwt начинаю

---
+++ src/main/java/Dom/project/Web_layer/config/JwtAuthenticationFilter.java
@@ -0,0 +1,55 @@
+package Dom.project.Web_layer.config;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import Dom.project.Application_layer.auth.AuthService;
+import Dom.project.Web_layer.utils.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final AuthService authService;
+
+    public JwtAuthenticationFilter(JwtUtils jwtUtils, AuthService authService) {
+        this.jwtUtils = jwtUtils;
+        this.authService = authService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        
+        final String authHeader = request.getHeader("Authorization");
+        
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = authHeader.substring(7);
+        String email = jwtUtils.extractEmail(jwt);
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (jwtUtils.validateToken(jwt)) {
+                // Создаем объект аутентификации (без проверки пароля, т.к. токен валидный)
+                UsernamePasswordAuthenticationToken authToken = 
+                    new UsernamePasswordAuthenticationToken(email, null, new ArrayList<>());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+}
